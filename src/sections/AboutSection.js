c0d554ef038582d1620f2a571dae6e12c32e7306
Laura Nina <170397lau@gmail.com>
2024-11-14 17:31:04 -0300

    los cambios realizados

--- src/sections/AboutSection.js
+++ src/sections/AboutSection.js
@@ -1,9 +1,23 @@
 // src/sections/AboutSection.js
+import cinta from './img/imagen/cinta.png';
 export default function AboutSection() {
   return (
     <section>
       <h2>Acerca de Nosotros</h2>
-      <p>Conoce más sobre nuestro gimnasio, nuestra misión y visión para ayudarte a alcanzar tus metas.</p>
+      <p>Somos Nombre del Gimnasio, nuestra misión es trascender el simple hecho de ejercitar. Buscamos transformar vidas, una repetición a la vez. Creemos en el poder del movimiento para liberar tu potencial físico y mental. Nuestro compromiso es brindarte un espacio donde no solo entrenes tu cuerpo, sino que también cultives una mente fuerte y un espíritu invencible. Queremos que al cruzar nuestras puertas, te sientas parte de una comunidad que te apoya y motiva a alcanzar tus metas.
+
+
+
+¿Te imaginas ser más fuerte, más saludable y más seguro de ti mismo? En Nombre de tu Gimnasio, esa visión es nuestra realidad. Queremos ser tu compañero en este viaje hacia una mejor versión de ti. Te ofreceremos más que solo máquinas y pesas; te proporcionaremos las herramientas, el conocimiento y la inspiración para que logres tus objetivos más ambiciosos. Con nosotros, no solo alcanzarás tus metas, sino que las superarás.
+
+
+
+Sabemos que el éxito de tu entrenamiento depende de las herramientas que tengas a tu disposición. Por eso, hemos equipado nuestro gimnasio con la tecnología más avanzada y las máquinas más eficientes del mercado. Nuestro equipo de última generación te permitirá trabajar todos los grupos musculares de forma segura y efectiva. Además, contamos con un staff de profesionales altamente capacitados que te brindarán la orientación y el apoyo personalizado que necesitas para maximizar tus resultados. En nuestro gimnasio, tienes todo lo que necesitas para alcanzar tus objetivos</p>
+   
+   <div>
+   <img src={cinta}/>
+   </div>
+   
     </section>
   );
 }
