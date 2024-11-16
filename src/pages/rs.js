3db5726a28bc3b44f57ab2b39658c62671d0251e
JuanCardozo52 <juancardozo52@gmail.com>
2024-11-16 01:53:10 -0300

    agrego section

---
+++ src/pages/rs.js
@@ -0,0 +1,9 @@
+import RsSection from '../sections/RsSectiontSection';
+
+export default function Contact() {
+  return (
+    <main>
+      <RsSection />
+    </main>
+  );
+}
---
+++ src/sections/RsSection.js
@@ -0,0 +1,10 @@
+export default function RsSection() {
+    return (
+      <section>
+
+         <h3>Aca van las RRSS</h3>
+        
+      </section>
+    );
+  }
+  
