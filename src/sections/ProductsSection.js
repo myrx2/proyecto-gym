0da585627c6f19308e5b07c8cf07b2e3ae591559
locotoe <moran.pablo48@gmail.com>
2024-11-19 19:21:09 -0300

    terminando tarjetas

--- src/sections/ProductsSection.js
+++ src/sections/ProductsSection.js
@@ -2,9 +2,11 @@
 import React,{ useState} from 'react';
 import PlanCard from '../components/PlanCard';
 import planData from '../utils/planData';
+import CardSection from '../components/CardSection';
 
 const ProductsSection = () => {
   return (
+    <>
     <section className="products-section">
       {planData.map((plan, index) => (
         <PlanCard
@@ -16,53 +18,10 @@ const ProductsSection = () => {
         />
       ))}
     </section>
+    <CardSection />
+
+    </>
   );
 };
 
-//Seccion tarjetas//
-
-const CardSection = () => {
-  const cardsData = new Array (20).fill(null).map((_, index) =>({
-    id:index,
-    image: `https://via.placeholder.com/150?text=Imagen+${index + 1}`,
-    description: `Descripción de la tarjeta ${index + 1}`,
-    price: `$${(index + 1) * 10}`,
-    liked: false,
-  }));
-
-  const handleLike = (id) => {
-    setCardsData(prevState =>
-      prevState.map(card =>
-        card.id === id ? {...card, liked: !card.liked}: card
-      )
-    );
-  };
-
-  const [cardsDataState, setCardsData] = useState(cardsData);
-
-  return (
-    <section className='card-section'>
-      <div className='card-grid'>
-        {cardsDataState.map(card => (
-          <div className='card' key={card.id}>
-            <div className='card-image-container'>
-              <img src={card.image} alt={`Card ${card.id}`} className="card-image" />
-              <button
-                className={`like-button ${card.liked ? 'liked' : ''}`}
-                onClick={() => handleLike(card.id)}
-              >
-                ❤️
-              </button>
-            </div>
-            <div className='card-details'>
-              <p className='card-description'>{card.description}</p>
-              <p className='card-price'>{card.price}</p>
-            </div>
-            </div>
-        ))}
-      </div>
-    </section>
-  );
-};
-
-export {ProductsSection, CardSection};
+export {ProductsSection};
