ed38e51527060b2623b7ec0368bfabca3cad58f2
deborah-ajalla <deborahajalla@gmail.com>
2024-11-20 15:54:10 -0300

    se completan estilos

--- src/sections/ContactSection.js
+++ src/sections/ContactSection.js
@@ -3,65 +3,58 @@ import styles from '../styles/contactSection.module.css';
 export default function ContactSection() {
     return (
       <section className={styles.contactSection}>
-         <h2>Contacto</h2>
-        
-         <p className= {styles.description}> ➡  to do ... : <br></br>
-             🔸rrss arriba <br></br>
-             🔸 agregar más opciones en select <br></br>  
-             🔸estilo <br></br>
-             
-         </p>
-     
+         <h2 className={styles.title}>Contacto</h2>
+           
          {/* frase motivadora */}
-         <p>
-            El camino hacia el éxito no es fácil, pero siempre vale la pena... <br></br> 
-            Recordá que los desafíos son lo que hacen la vida interesante, y VOS TAMBIÉN PODÉS!! <br></br>
-            EMPEZÁ HOY a transformar tu vida, tu cuerpo, tu estilo de vida! <br></br>
+         <p className= {styles.description}>
+            El camino hacia el éxito no es fácil, pero siempre vale la pena... 
+            Recordá que los desafíos son lo que hacen la vida interesante, y VOS TAMBIÉN PODÉS!! 
+            EMPEZÁ HOY a transformar tu vida, tu cuerpo, tu estilo de vida! 
             Completá con tus datos asi nos ponemos en contacto y te asesoramos para que empieces a transformar tu rutina!!
          </p>
 
           {/* formu */}
         <div>
-             <form>
-                <fieldset>
+             <form className= {styles.formulario}>
+                <fieldset className={styles.contenido}>
                    <label htmlFor='nombre'> Nombre: </label>
-                   <input type='text' name="nombre" id='nombre'>                
+                   <input className={styles.input} type='text' name="nombre" id='nombre'>                
                    </input>
                 </fieldset>
           
-                <fieldset>
+                <fieldset className={styles.contenido}>
                    <label htmlFor='apellido'> Apellido: </label>
-                   <input type='text' name="apellido" id='apellido'>               
+                   <input className={styles.input} type='text' name="apellido" id='apellido'>               
                    </input>
                 </fieldset>
 
-                <fieldset>
+                <fieldset className={styles.contenido}>
                    <label htmlFor='mail'> Email: </label>
-                   <input type='email'name="mail" id='mail'>               
+                   <input className={styles.input} type='email'name="mail" id='mail'>               
                    </input>
                 </fieldset>
 
-                <fieldset>
+                <fieldset className={styles.contenido}>
                    <label htmlFor='celu'> Celular: </label>
-                   <input type='tel' name="celu" id='celu'>               
+                   <input className={styles.input} type='tel' name="celu" id='celu'>               
                    </input>
                 </fieldset>
 
-                <fieldset>
+                <fieldset className={styles.contenido}>
                    <label htmlFor="select"> Elegir una opción:</label>
-                   <select>
+                   <select className={styles.input}>
                       <option selected value="info">Info</option>
                       <option value="productos">Productos</option>
                       <option  value="planes">Planes</option>
                    </select>
                 </fieldset>
 
-                <div>
+                <div className={styles.textarea}>
                    <label> Ingrese Aquí su Consulta: </label>
-                   <textarea> </textarea>
+                   <textarea className={styles.input}> </textarea>
                 </div>
 
-                <button className={styles.ctaButton}> Enviar </button>
+                <button className={styles.button}> Enviar </button>
              </form>
         </div>
       </section>
