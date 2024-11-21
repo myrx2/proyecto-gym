7ddda93b772267a3bd7a419f63822640343b1485
JuanCardozo52 <juancardozo52@gmail.com>
2024-11-21 06:09:45 -0300

    se agregan images y se crean cards de profes

--- src/sections/Profes.Section.js
+++ src/sections/Profes.Section.js
@@ -1,17 +1,33 @@
+import ProfesCards from '@/components/Profes';
 import styles from '../styles/ProfesSection.module.css';
+import profesData from '@/utils/profesData';
 
 export default function ProfesSection () {
     return (
-       <section className={styles.ProfesSection}>
-         <h2 className={styles.title}> Nuestros Profes </h2>
-         <p className={styles.description}> Contamos con un Equipo de Profesionales de Primer Nivel que están capacitados para brindarte las clases más completas y prácticas! 
-             Todos nuestros Profes se perfeccionan  constantemente para traerte las últimas tendencias y las mejores rutinas. No lo dudes y vení a entrenar con nosotros!!! </p>
+        <> 
+            <section className={styles.ProfesSection}>
+                <h2 className={styles.title}> Nuestros Profes </h2>
+                <p className={styles.description}> Contamos con un Equipo de Profesionales de Primer Nivel que están capacitados
+                     para brindarte las clases
+                    más completas y   prácticas! 
+                    Todos nuestros Profes se perfeccionan  constantemente para traerte las últimas tendencias y las mejores rutinas. No lo dudes y vení a entrenar con nosotros!!! </p>
 
-        <div>
+            </section>
 
-        </div>
-       </section>
-        
+            <section>
+                <div className="owl-carousel ">
+                    {profesData.map((clase, index) => (
+                        <ProfesCards
+                           key={index}
+                           image={clase.image}
+                           title={clase.title}
+                           description={clase.description}
+                        />
+                    ))}
+                </div>
+                   
+            </section>
+       </>   
 
     );
 }
---
+++ src/utils/profesData.js
@@ -0,0 +1,48 @@
+const profesData = [
+    {
+      key:"0",
+      image: "../images/profe-boxeo.jpg",
+      title: "Ariel Ramírez",
+      description:
+       "Profe de Boxeo",
+    },
+    {
+      key:"1",
+      image: "../images/profe-crossfit.jpg",
+      title: "Nadia Pacheco" ,
+      description:
+       "Profe de Crossfit",
+    },
+    {
+      key:"2",
+      image: "../images/profes-zumba.jpg",
+      title: "Alejandra y Martina Escobar",
+      description:
+        "Profes de Zumba",
+    },
+    {
+      key:"3",
+      image: "../images/profe-spining.jpg",
+      title: "Pablo Díaz",
+      description:
+        "Profe de Spinning",
+    },
+    {
+      key:"4",
+      image: "../images/profe-pilates.jpg",
+      title: "Pía Mendoza",
+      description:
+        "Profe de Pilates",
+    },
+    {
+      key:"5",
+      image: "../images/profe-yoga.jpg",
+      title: "Mariah Nikel",
+      description:
+        "Profe de Yoga",
+    }
+       
+  ];
+
+   export default profesData;
+  
