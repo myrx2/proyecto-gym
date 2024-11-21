77e665040431507b4f6e701f5a75a3f097dd5fcf
lokur <lokura.x2@gmail.com>
2024-11-21 11:33:41 -0300

    About-nina

--- src/sections/AboutSection.js
+++ src/sections/AboutSection.js
@@ -1,37 +1,33 @@
 import React from "react";
 import ClasesCard from "../components/ClasesCard";
 import clasesData from "../utils/clasesData";
+import styles from '../styles/about.module.css';
 
 export default function AboutSection() {
   return (
     <>
-      <section className="sectionUs">
-        <h2>Acerca de Nosotros</h2>
-        <h3>
-          En <strong>Gym</strong>, nuestra misión es ayudarte a transformar tu
-          vida a través del ejercicio
-        </h3>
-        <ul>
-          <li>
-            Buscamos ofrecerte un espacio donde puedas entrenar tu cuerpo y
-            fortalecer tu mente, rodeado de una comunidad que te apoya.
-          </li>
-          <li>
-            Nos enfocamos en brindarte las mejores herramientas y la motivación
-            necesaria para alcanzar tus objetivos, con un equipo de
-            profesionales que te guiarán en cada paso del camino.
-          </li>
-          <li>
-            Contamos con equipos de última tecnología para ofrecerte un
-            entrenamiento seguro y efectivo, adaptado a tus necesidades.
-          </li>
-        </ul>
+     <section>
+      <h2>Acerca de Nosotros</h2>
+      <h3>
+        En <strong>Gym</strong>, nuestra misión es ayudarte a transformar tu vida a través del ejercicio
+      </h3>
+      <ul>
+        <li>
+          Buscamos ofrecerte un espacio donde puedas entrenar tu cuerpo y fortalecer tu mente, rodeado de una comunidad que te apoya.
+        </li>
+        <li>
+          Nos enfocamos en brindarte las mejores herramientas y la motivación necesaria para alcanzar tus objetivos, 
+          con un equipo de profesionales que te guiarán en cada paso del camino.
+        </li>
+        <li>
+          Contamos con equipos de última tecnología para ofrecerte un entrenamiento seguro y efectivo, adaptado a tus necesidades.
+        </li>
+      </ul>
+      <div className={styles.corre}>
+        <img className={styles.imagenGimnasio} src="/images/cinta.jpg" alt="Imagen del Gimnasio" />
+      </div>
+    </section>
 
-        <div className="imageUs">
-          <img src="/images/cinta.jpg" alt="Imagen del Gimnasio"/>
-        </div>
-
-      </section>
 
       <section>
         <h2>Nuestras clases</h2>
