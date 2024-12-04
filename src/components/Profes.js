4f0d95313f40f4d798e1b9e84986cea0f9b60d94
deborah-ajalla <deborahajalla@gmail.com>
2024-12-04 02:55:08 -0300

    fix section profes

--- src/components/Profes.js
+++ src/components/Profes.js
@@ -2,23 +2,21 @@
 import styles from "../styles/ProfesSection.module.css";
 
 const ProfesCards = ({ image, title, description }) => {
-  return (
-    <div className={styles["card-container"]}>
-         <div className={styles["card"]}>
-             <div className={styles["card-inner"]}>
-                 <div className={styles["card-front"]}>
-                     <div className="item">
-                         <img src={image} alt="profes"/>
-                     </div>
-                 </div>
-                 <div className={styles["card-back"]}>
-                     <h3>{title}</h3>
-                     <p>{description}</p>
-                 </div>
+   return (
+     <div className={styles["container"]}>
+          <div className={styles["card"]}>
+             <div className="card-imagen">
+                  <img src={image} alt="profes"/>
              </div>
-         </div>
-    </div>
-  );
-};
+                        
+              <div className={styles["card-body"]}>
+                  <h3 className={styles["card-title"]}>{title}</h3>
+                  <p className="card-description">{description}</p>
+              </div>       
+          </div>  
+     </div>
+   );
+ };
+
 
 export default ProfesCards;
